ca190592afdfdb24330f861b1489e078f76f41ea
dreedup <andredransom@gmail.com>
2023-06-03 16:11:53 +0100

    Eight ball application created

--- server.js
+++ server.js
@@ -18,6 +18,36 @@ app.get('/greeting/:name', (req, res) => {
     res.send(tipAmount.toString());
   });
 
+  const magic8BallResponses = [
+    "It is certain.",
+    "It is decidedly so.",
+    "Without a doubt.",
+    "Yes - definitely.",
+    "You may rely on it.",
+    "As I see it, yes.",
+    "Most likely.",
+    "Outlook good.",
+    "Yes.",
+    "Signs point to yes.",
+    "Reply hazy, try again.",
+    "Ask again later.",
+    "Better not tell you now.",
+    "Cannot predict now.",
+    "Concentrate and ask again.",
+    "Don't count on it.",
+    "My reply is no.",
+    "My sources say no.",
+    "Outlook not so good.",
+    "Very doubtful."
+  ];
+  
+  app.get('/magic/:question', (req, res) => {
+    const question = req.params.question.replace(/%20/g, ' ');
+    const randomResponse = magic8BallResponses[Math.floor(Math.random() * magic8BallResponses.length)];
+    const responseHtml = `<h1>${question}</h1><h1>${randomResponse}</h1>`;
+    res.send(responseHtml);
+  });
+
 app.listen(3000, function () {
     console.log('Listening on port 3000');
 });
