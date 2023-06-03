0b4d8a333957e1c9a9a8126f23d2273cfe877d3a
dreedup <andredransom@gmail.com>
2023-06-03 16:09:40 +0100

    Tip Calculator express application created

--- server.js
+++ server.js
@@ -5,6 +5,19 @@ const app = express();
 app.get('/', function (req, res) {res.send('<h1>Hello World!</h1>')
 })
 
+app.get('/greeting/:name', (req, res) => {
+    const name = req.params.name;
+    const greeting = `Hello, ${name}! It's so great to see you!`;
+    res.send(greeting);
+  })
+
+  app.get('/tip/:total/:tipPercentage', (req, res) => {
+    const total = parseFloat(req.params.total);
+    const tipPercentage = parseFloat(req.params.tipPercentage);
+    const tipAmount = (total * tipPercentage) / 100;
+    res.send(tipAmount.toString());
+  });
+
 app.listen(3000, function () {
     console.log('Listening on port 3000');
 });
